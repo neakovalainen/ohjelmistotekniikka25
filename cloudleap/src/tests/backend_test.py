8d98a932907d151fb72c9ec8ca1fc6bc928c80c6
neakovalainen <neakovalaine@gmail.com>
2025-04-08 15:11:24 +0300

    updated tests

--- cloudleap/src/tests/backend_test.py
+++ cloudleap/src/tests/backend_test.py
@@ -5,6 +5,8 @@ from objects import Meow, PointCollector, CloudSpawner
 class TestBackend(unittest.TestCase):
     def setUp(self):
         self.player = Meow(500, 550)
+        self.game = InitializeGame
+        self.game.game_started = True
 
     def test_player_x_coordinate_initialized_correctly(self):
         self.assertEqual((self.player.x, self.player.y), (500, 550))
@@ -16,20 +18,20 @@ class TestBackend(unittest.TestCase):
 
     def test_cloud_spawns_outside_screen_if_dissappeared_from_left(self):
         self.cloud = CloudSpawner(-600, 500)
-        self.cloud.cloudposition()
+        self.cloud.cloudposition(self.game.game_started)
         self.assertTrue(1280 <= self.cloud.x <= 2000)
     
     def test_cloud_moves_left_while_on_screen(self):
         self.cloud = CloudSpawner(100, 500)
-        self.cloud.cloudposition()
+        self.cloud.cloudposition(self.game.game_started)
         self.assertEqual(self.cloud.x, 96)
 
     def test_energy_drink_spawns_outside_screen_if_disappeared_from_left(self):
         self.energy = PointCollector(-3, 500)
-        self.energy.obtainableposition()
+        self.energy.obtainableposition(self.game.game_started)
         self.assertTrue(1280 <= self.energy.x <= 2000)
 
     def test_energy_drink_moves_left_while_on_screen(self):
         self.energy = PointCollector(500, 550)
-        self.energy.obtainableposition()
+        self.energy.obtainableposition(self.game.game_started)
         self.assertEqual(self.energy.x, 496)
